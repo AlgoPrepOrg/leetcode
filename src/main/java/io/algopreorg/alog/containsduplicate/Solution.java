a8595395186c59c2cf31af5b5a81bca7a8fe7146
Roman Novosad <roman.novosad@yahoo.com>
2023-12-17 12:14:55 +0100

    solution to contains duplicate using HashSet

--- src/main/java/io/algopreorg/alog/containsduplicate/Solution.java
+++ src/main/java/io/algopreorg/alog/containsduplicate/Solution.java
@@ -1,8 +1,17 @@
 package io.algopreorg.alog.containsduplicate;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
     public boolean containsDuplicate(int[] nums) {
+        Set<Integer> integerSet = new HashSet<>();
+        for (int num : nums) {
+            if (!integerSet.add(num)) {
+               return true;
+            }
+        }
         return false;
     }
 }
