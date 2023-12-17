182401a27288859a4f6151074bf33a202bd5ccc8
Roman Novosad <roman.novosad@yahoo.com>
2023-12-17 12:50:49 +0100

    solution to array concatenation using System.arraycopy

--- src/main/java/io/algopreorg/alog/concatenationofarray/Solution.java
+++ src/main/java/io/algopreorg/alog/concatenationofarray/Solution.java
@@ -3,6 +3,9 @@ package io.algopreorg.alog.concatenationofarray;
 public class Solution {
 
     public int[] getConcatenation(int[] nums) {
-        return new int[]{};
+        int[] newArray = new int[2 * nums.length];
+        System.arraycopy(nums, 0, newArray, 0, nums.length);
+        System.arraycopy(nums, 0, newArray, nums.length, nums.length);
+        return newArray;
     }
 }
