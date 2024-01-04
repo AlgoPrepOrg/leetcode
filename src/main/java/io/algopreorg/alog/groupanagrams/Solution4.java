41ff270f994a166a58c8d0b958286e503fbe92a9
vitalish <vitalish@mac.local>
2024-01-04 21:42:22 +0200

    49. Group Anagrams

--- src/main/java/io/algopreorg/alog/groupanagrams/Solution4.java
+++ src/main/java/io/algopreorg/alog/groupanagrams/Solution4.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Solution4 {
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> result = new HashMap();
+        Map<String, List<String>> result = new HashMap<>();
         for (var str : strs) {
             String hash = getHash(str);
             if (result.containsKey(hash)) {
