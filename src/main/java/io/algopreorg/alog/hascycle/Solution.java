ef589d1e7234ab16fb79478b906f454a5ef1bd5d
PolukovY <73576438+PolukovY@users.noreply.github.com>
2024-03-12 22:59:20 +0200

    Two Pointers[141. Linked List Cycle] (#37)
    * Two Pointers
    - 141. Linked List Cycle
    
    * Two Pointers
    - 26. Remove Duplicates from Sorted Array
    
    * Two Pointers
    - 27. Remove Element.
    
    ---------
    
    Co-authored-by: levik <zhenya12@gmail.com>

---
+++ src/main/java/io/algopreorg/alog/hascycle/Solution.java
@@ -0,0 +1,29 @@
+package io.algopreorg.alog.hascycle;
+
+public class Solution {
+
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
