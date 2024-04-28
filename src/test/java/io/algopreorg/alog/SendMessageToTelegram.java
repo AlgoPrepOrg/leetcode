968f78a1fdc3c3e1c9ee1272dc6b4e37d4194860
Yevgen.P <yevgen.polukov@gmail.com>
2024-04-28 18:45:16 +0300

    add MotivationQuotes and refactor tests.

--- src/test/java/io/algopreorg/alog/SendMessageToTelegram.java
+++ src/test/java/io/algopreorg/alog/SendMessageToTelegram.java
@@ -3,12 +3,7 @@ package io.algopreorg.alog;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Objects;
+import static io.algopreorg.alog.TelegramUtils.sendMessage;
 
 class SendMessageToTelegram {
 
@@ -16,54 +11,24 @@ class SendMessageToTelegram {
     private static final String CHAT_ID = "TELEGRAM_CHAT_ID";
     private static final String NEW_PULL_REQUEST_OPENED_HTTPS_GITHUB_COM_ALGO_PREP_ORG_LEET_CODE_PULLS =
             "New Pull Request [opened](https://github.com/AlgoPrepOrg/leetcode/pulls)";
-    public static final String TELEGRAM_SEND_MESSAGE_API_URL = "https://api.telegram.org/bot%s/sendMessage";
-    public static final String POST = "POST";
-    public static final String CONTENT_TYPE = "Content-Type";
-    public static final String APPLICATION_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
 
     @Tag("sendTelegramMessage")
     @Test
     void sendTelegramMessage() throws Exception {
         String botToken = System.getenv(BOT_TOKEN);
         String chatId = System.getenv(CHAT_ID);
+        sendMessage(botToken, chatId, NEW_PULL_REQUEST_OPENED_HTTPS_GITHUB_COM_ALGO_PREP_ORG_LEET_CODE_PULLS);
+    }
 
-        if (Objects.nonNull(botToken) && Objects.nonNull(chatId)) {
-            String telegramMessageApiUrl = String.format(TELEGRAM_SEND_MESSAGE_API_URL, botToken);
+    @Tag("sendTelegramMessage")
+    @Test
+    void generateRandomQuotes() throws Exception  {
+        String botToken = System.getenv(BOT_TOKEN);
+        String chatId = System.getenv(CHAT_ID);
 
-            String parameters = "chat_id=" + chatId +
-                    "&parse_mode=MarkdownV2" +
-                    "&text=" + NEW_PULL_REQUEST_OPENED_HTTPS_GITHUB_COM_ALGO_PREP_ORG_LEET_CODE_PULLS;
+        var motivationQuotes = new MotivationQuotes();
+        String randomQuote = motivationQuotes.getRandomQuote();
 
-            HttpURLConnection connection = null;
-            try  {
-                var url = new URL(telegramMessageApiUrl);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod(POST);
-                connection.setDoOutput(true);
-                connection.setRequestProperty(CONTENT_TYPE, APPLICATION_X_WWW_FORM_URLENCODED);
-
-                try (var outputStream = new DataOutputStream(connection.getOutputStream())) {
-                    outputStream.writeBytes(parameters);
-                    outputStream.flush();
-                }
-
-                int responseCode = connection.getResponseCode();
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    try (var reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                        String response = reader.lines()
-                                .reduce(new StringBuilder(), StringBuilder::append, StringBuilder::append).toString();
-                        System.out.println("Message sent successfully. Response: " + response);
-                    }
-                } else {
-                    System.out.println("Failed to send message. Response code: " + responseCode);
-                }
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            }
-        } else {
-            System.out.println("Skip sendMessage to telegram  botToken & chatId is null.");
-        }
+        sendMessage(botToken, chatId, randomQuote);
     }
 }
