d26de3d42ffd5e6d740d378f6dae223fdddde3b2
romanovosad87 <roman.novosad@yahoo.com>
2024-05-17 08:48:06 +0200

    - Best Time to Buy and Sell Stock - Contains Duplicate - Number of Sub arrays of Size K and Average Greater than or Equal to Threshold

---
+++ src/main/java/io/algopreorg/alog/slicingwindow/containsduplicate/SolutionRoman.java
@@ -0,0 +1,22 @@
+package io.algopreorg.alog.slicingwindow.containsduplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://leetcode.com/problems/contains-duplicate-ii/description/
+
+public class SolutionRoman {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        int j = 0;
+        for (int num : nums) {
+            if (set.size() > k) {
+                set.remove(nums[j++]);
+            }
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/io/algopreorg/alog/slicingwindow/numberfosubarrays/SolutionRoman.java
@@ -0,0 +1,28 @@
+package io.algopreorg.alog.slicingwindow.numberfosubarrays;
+
+// https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/description/
+
+public class SolutionRoman {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int result = 0;
+        int sum = 0;
+        int target = k * threshold;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i < k) {
+                sum += arr[i];
+            } else {
+                if (sum >= target) {
+                    result++;
+                }
+                sum += arr[i] - arr[i - k];
+            }
+        }
+
+        if (sum >= target) {
+            result++;
+        }
+
+        return result;
+    }
+}
