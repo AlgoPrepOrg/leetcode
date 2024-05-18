2a450a14af41fca6b7bb2b9fa5eade6b3f9c8115
Yevgen.P <yevgen.polukov@gmail.com>
2024-05-18 21:38:39 +0300

    add escape to string.

--- src/test/java/io/algopreorg/alog/TelegramUtils.java
+++ src/test/java/io/algopreorg/alog/TelegramUtils.java
@@ -15,6 +15,8 @@ public class TelegramUtils {
         if (Objects.nonNull(botToken) && Objects.nonNull(chatId)) {
             String telegramMessageApiUrl = String.format(TELEGRAM_SEND_MESSAGE_API_URL, botToken);
 
+            text = escapeString(text);
+
             String parameters = "chat_id=" + chatId +
                     "&parse_mode=MarkdownV2" +
                     "&text=" + text;
@@ -25,4 +27,44 @@ public class TelegramUtils {
             System.out.println("Skip sendMessage to telegram  botToken & chatId is null.");
         }
     }
+
+    public static String escapeString(String input) {
+        StringBuilder escapedString = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            switch (c) {
+                case '\"':
+                    escapedString.append("\\\"");
+                    break;
+                case '\\':
+                    escapedString.append("\\\\");
+                    break;
+                case '\b':
+                    escapedString.append("\\b");
+                    break;
+                case '\f':
+                    escapedString.append("\\f");
+                    break;
+                case '\n':
+                    escapedString.append("\\n");
+                    break;
+                case '\r':
+                    escapedString.append("\\r");
+                    break;
+                case '\t':
+                    escapedString.append("\\t");
+                    break;
+                case '(':
+                    escapedString.append("\\(");
+                    break;
+                case ')':
+                    escapedString.append("\\)");
+                    break;
+                default:
+                    escapedString.append(c);
+                    break;
+            }
+        }
+        return escapedString.toString();
+    }
+
 }
